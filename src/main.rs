af3dbce0312b2b9eab23197db7884c933d1af3a5
tangwu <tangwu@toponegames.cn>
2022-11-04 18:46:27 +0800

    编写RoaringSet（尚未测试）

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 mod roaring_bitmap;
-use roaring_bitmap::RoaringBitMap;
-use std::collections::BTreeSet;
-use roaring_set;
+mod roaring_set;
+// use roaring_bitmap::RoaringBitMap;
+// use std::collections::BTreeSet;
+use roaring_set::RoaringSet;
 fn main() {
-    let mut roaring_bitmap = RoaringBitMap::new();
-    roaring_bitmap.add((1<<16)*2+1);
-    let mut tree_set:BTreeSet<u16> = BTreeSet::new();
-    tree_set.insert(1);
-    tree_set.insert(2);
-    tree_set.insert(2);
-    println!("tree_set.len():{}", tree_set.len());
+    // let mut roaring_bitmap = RoaringBitMap::new();
+    // roaring_bitmap.add((1<<16)*2+1);
+    // println!("tree_set.len():{}", tree_set.len());
+    let mut set = RoaringSet::new();
+    set.add(1);
+    set.add(2);
+    set.add(2);
+    set.add(3);
+    set.remove(2);
+    println!("set.len():{}", set.len());
 }
--- src/roaring_set.rs
+++ src/roaring_set.rs
@@ -1,35 +1,75 @@
 use std::collections::BTreeSet;
 
 pub struct RoaringSet{
-    use_vec:    bool,
+    use_tree_set:    bool,
     vec:        Vec<bool>,
     tree_set:   BTreeSet<u16>,
-    mid_size:   u16,
+    mid_size:   usize,
     num:        usize,
 }
 
 impl RoaringSet{
-    pub fn new(){
+    pub fn new() -> RoaringSet{
         return RoaringSet{
-            use_vec:    true,
-            vec:        Vec::new(),
-            tree_set:   BTreeSet::new(),
-            mid_size:   1<<14,
+            use_tree_set:   true,
+            vec:            Vec::new(),
+            tree_set:       BTreeSet::new(),
+            mid_size:       1<<14,
+            num:            0,
         };
     }
 
     pub fn add(&mut self, value: u16){
-        if self.use_vec{
-            if value >= vec.len(){
-                vec.resize(value + 1);
-            }
-            if !vec[value]{
-                num = num + 1;
-            }
-            vec[value] = true;
+        if self.use_tree_set{
+            self.tree_set.insert(value);
+            self.num = self.tree_set.len();
         }else{
-            tree_set.insert(value);
-            num = tree_set.len()
+            let usize_val = value as usize;
+            if usize_val >= self.vec.len(){
+                self.vec.resize(usize_val + 1, false);
+            }
+            if !self.vec[usize_val]{
+                self.num = self.num + 1;
+            }
+            self.vec[usize_val] = true;
         }
+        if self.num > self.mid_size && self.use_tree_set{
+            //change to Vec
+            self.vec.resize(self.num, false);
+            for i in 0..self.vec.len(){
+                self.vec[i] = false;
+            }
+            for elem in &self.tree_set{
+                self.vec[*elem as usize] = true;
+            }
+        }
+    }
+
+    pub fn remove(&mut self, value: u16){
+        if self.use_tree_set{
+            self.tree_set.remove(&value);
+            self.num = self.tree_set.len();
+        }else{
+            let usize_val = value as usize;
+            if usize_val < self.vec.len(){
+                if self.vec[usize_val]{
+                    self.num = self.num - 1;
+                }
+                self.vec[usize_val] = false;
+            }
+        }
+        if self.num < (self.mid_size>>1) && !self.use_tree_set{
+            //change to BTreeSet
+            self.tree_set.clear();
+            for i in 0..self.vec.len(){
+                if self.vec[i]{
+                    self.tree_set.insert(i as u16);
+                }
+            }
+        }
+    }
+
+    pub fn len(&self) -> usize{
+        return  self.num;
     }
 }
