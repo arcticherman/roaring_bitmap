9e0245238c202d5e9eafcdf8e6e65a397904446a
tangwu <710102161@qq.com>
2022-11-13 21:00:03 +0800

    添加RoaringBitMap和RoaringBitMap64的测试代码

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,30 @@ mod roaring_bitmap64;
 mod roaring_set;
 mod vec_set;
 use roaring_bitmap64::RoaringBitMap64;
-// use roaring_set::RoaringSet;
-fn main() {
-    let mut roaring_bitmap = RoaringBitMap64::new();
-    // for i in 1..32768{
-    //     roaring_bitmap.add(i);
-    // }
-    // for i in 8190..32768{
-    //     roaring_bitmap.remove(i);
-    // }
-    // println!("roaring_bitmap.len() :{} ", roaring_bitmap.len());
+use roaring_bitmap::RoaringBitMap;
 
+#[derive(Debug)]
+struct  Chinese{
+    name:   String,
+}
+fn main() {
+    let mut roaring_bitmap = RoaringBitMap::new();
+    roaring_bitmap.add(1);
     roaring_bitmap.add(1);
     roaring_bitmap.add(2);
-    roaring_bitmap.add(65534);
-    roaring_bitmap.add(65535);
-    roaring_bitmap.add(18446744073709551613);
-    roaring_bitmap.add(18446744073709551614);
-    roaring_bitmap.add(18446744073709551615);
-    println!("roaring_bitmap :{:?} ", roaring_bitmap.values());
+    roaring_bitmap.add(3);
+    roaring_bitmap.add(4294967293);
+    roaring_bitmap.add(4294967294);
+    roaring_bitmap.add(4294967295);
+    println!("roaring_bitmap: {:?} ", roaring_bitmap.values());
+
+    let mut roaring_bitmap64 = RoaringBitMap64::new();
+    roaring_bitmap64.add(1);
+    roaring_bitmap64.add(1);
+    roaring_bitmap64.add(2);
+    roaring_bitmap64.add(3);
+    roaring_bitmap64.add(18446744073709551613);
+    roaring_bitmap64.add(18446744073709551614);
+    roaring_bitmap64.add(18446744073709551615);
+    println!("roaring_bitmap64: {:?} ", roaring_bitmap64.values());
 }
